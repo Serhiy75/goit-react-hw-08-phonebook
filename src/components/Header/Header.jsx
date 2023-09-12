51078907d9fdafd941f0f9faca0ffcf4fadfbc83
sstfnk3107 <s.stefanyk75@gmail.com>
2023-09-12 03:15:11 +0300

    123

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,38 @@
-import { Navigation } from '../Navigation/Navigation';
-import { UserMenu } from '../UserMenu/UserMenu';
-import { AuthNav } from '../AuthNav/AuthNav';
-import { useAuth } from 'hooks';
-import css from './AppBar.module.css';
+import { Suspense } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, Outlet } from 'react-router-dom';
+import { logout } from 'redux/auth/operations';
+import { selectUser } from 'redux/auth/selectors';
 
 export const Header = () => {
-  const { isLogin } = useAuth();
+  const dispatch = useDispatch();
+  const user = useSelector(selectUser);
 
   return (
-    <header className={css.header}>
-      <Navigation />
-      {isLogin ? <UserMenu /> : <AuthNav />}
-    </header>
+    <>
+      <header>
+        <p>Welocome: {user.name}</p>
+        <button onClick={() => dispatch(logout())}>Logout</button>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Register</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contacts">Contacts</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Outlet />
+      </Suspense>
+    </>
   );
 };
