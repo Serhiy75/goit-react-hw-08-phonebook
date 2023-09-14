ea58c915ea0ef37877113b45c0a7dd3a0b1ddd2f
sstfnk3107 <s.stefanyk75@gmail.com>
2023-09-14 13:48:08 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,21 @@ import { Phonebook } from 'pages/Phonebook/Phonebook';
 import Login from 'pages/Login/Login';
 import Register from 'pages/Register/Register';
 import Home from 'pages/Home';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsRefreshing } from 'redux/auth/selectors';
-import { Header } from './Header/Header';
 import { useEffect } from 'react';
 import { refreshUser } from 'redux/auth/operations';
 import { Route, Routes } from 'react-router-dom';
 import { RestrictedRoute } from './RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute';
+import { Header } from './Header/Header';
 
-// const Header = lazy(() => import('../components/Header'));
+// const Header = lazy(() => import('./Header/Header'));
 // const Home = lazy(() => import('pages/Home'));
-// const Register = lazy(() => import('pages/Register'));
-// const Login = lazy(() => import('pages/Login'));
-// const Phonebook = lazy(() => import('pages/Phonebook'));
+// const Register = lazy(() => import('pages/Register/Register'));
+// const Login = lazy(() => import('pages/Login/Login'));
+// const Phonebook = lazy(() => import('pages/Phonebook/Phonebook'));
 
 export const App = () => {
   const dispatch = useDispatch();
